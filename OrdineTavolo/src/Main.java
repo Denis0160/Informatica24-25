ebe37b644cae2e473e341e2dc0d6a30ba436fe86
Denis <150442493+Denis0160@users.noreply.github.com>
2024-12-07 11:20:30 +0100

    modificato tavolo

--- OrdineTavolo/src/Main.java
+++ OrdineTavolo/src/Main.java
@@ -1,19 +1,75 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import static Tools.Utility.*;
 
-public class Main {
+public class  Main {
     public static void main(String[] Args){
         Scanner scanner=new Scanner(System.in);
+        boolean fine = true;
+        String[] opzioni={"GESTIONE PUB", "aggiungi tavolo","visualizza tavoli","occupa tavolo","fine"};
+        ArrayList<Tavolo> tavoli=new ArrayList<>();
+        do {
+            switch (Menu( opzioni, scanner )) {
+                case 1: {
+                    Tavolo tavolo= creaTavolo( scanner );
+                    if(tavolo!=null){
+                        tavoli.add( tavolo );
+                    }
+                    break;
+                }
+                case 2: {
+                    for(Tavolo x:tavoli){
+                        System.out.println(x.toString());
+                    }
+                    break;
+                }
+                case 3:{
+                    occupaTavolo( scanner,tavoli );
+                    break;
+                }
+
+                default: {
+                    fine = false;
+                }
+            }
+        }while (fine);
 
     }
 
     /**
      * metodo per la creazione dei tavoli
      */
-    private static void creaTavolo(){
-
+    private static Tavolo creaTavolo(Scanner scanner) {
+        Tavolo tavolo = null;
+        try {
+            System.out.println("Inserisci il numero di posti del tavolo:\n");
+            String nPosti1;
+            nPosti1 = scanner.nextLine();
+            int nPosti=Integer.parseInt( nPosti1 );
+            tavolo = new Tavolo(nPosti, true);
+        } catch (Exception e) {
+            System.out.println("Errore nella creazione del tavolo: " + e.getMessage());
+        }
+        return tavolo;
     }
 
-
-
+    private static void occupaTavolo(Scanner scanner, ArrayList<Tavolo> t){
+        System.out.println("Quante persone sono?\n");
+        String persone;
+        persone=scanner.nextLine();
+        int nPersone=Integer.parseInt( persone );
+        boolean trovato=false;
+        for(Tavolo i:t){
+            if(nPersone<=i.getnposti()){
+                System.out.println("Tavolo trovato!\n");
+                i.setLibero( false );
+                trovato=true;
+            }
+        }
+        if(trovato==false){
+            System.out.println("Non c'è un tavolo disponibile per queste persone\n");
+        }
+    }
 
 }
--- OrdineTavolo/src/Tavolo.java
+++ OrdineTavolo/src/Tavolo.java
@@ -1,43 +1,94 @@
 public class Tavolo {
+
+    /**
+     * contatore per il numero del tavolo
+     */
+    private static int numeroCounter = 1;
+
+    /**
+     * numero del tavolo
+     */
     private int numero;
+
+    /**
+     * numero di posti per tavolo
+     */
     private int nPosti;
+
+    /**
+     * numero massimo di posti per tavolo
+     */
     private  final int  nmax=6;
 
+    /**
+     * per stabilire se è libero o no
+     */
     private boolean libero;
 
-    public Tavolo(int numero, int nPosti, boolean libero)throws Exception{
+    /**
+     * costruttore del tavolo
+     * @param nPosti
+     * @param libero
+     * @throws Exception
+     */
+    public Tavolo( int nPosti, boolean libero)throws Exception{
 
-        if(nPosti>nmax){
-            throw new Exception("I posti massimi sono stati superati");
+        if(nPosti>nmax || nPosti < 0){
+            throw new Exception("I posti massimi sono stati superati ");
         }else{
             this.nPosti=nPosti;
         }
-        this.numero=numero;
         this.libero=libero;
+        this.numero = numeroCounter++;
     }
+
+    /**
+     * metodo per modificare il numero dei posti
+     * @param nPosti
+     */
     public void setnPosti(int nPosti){
         this.nPosti=nPosti;
     }
 
-
+    /**
+     * metodo per avere il numero dei posti
+     * @return numero posti
+     */
     public int getnposti(){
         return nPosti;
     }
 
-    public void setNumero(int numero){
-        this.numero=numero;
-    }
-
+    /**
+     * metodo per avere il numero del tavolo
+     * @return numero del tavolo
+     */
     public int getNumero(){
         return numero;
     }
 
+    /**
+     * metodo che modifica lo stato del tavolo
+     * @param libero
+     */
     public void setLibero(boolean libero){
         this.libero=libero;
     }
 
+    /**
+     * metodo get per vedere se il tavolo è libero
+     * @return true se il tavolo è libero false se è occcupato
+     */
     public boolean getLibero(){
         return libero;
+    }
+
+    /**
+     * metodo per stampare i tavoli
+     * @return Stringa formattata che stampa i dati del tavolo
+     */
+    @Override
+    public String toString(){
+        return String.format( "nPosti: %d  numero tavolo: %d   libero: %b",this.nPosti,this.numero,this.libero );
     }
 }
 
