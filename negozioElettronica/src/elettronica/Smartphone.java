b9ee226b86d6a2b5c779f3a8de129cef1e30eddb
Denis <150442493+Denis0160@users.noreply.github.com>
2025-03-25 10:20:46 +0100

    Add files via upload

---
+++ negozioElettronica/src/elettronica/Smartphone.java
@@ -0,0 +1,43 @@
+package elettronica;
+
+public class Smartphone extends ProdottoElettronico {
+    String modello;
+    int memoria;
+    final int prezzoRam = 10;
+
+    public Smartphone(String codiceProdotto, String marca, double prezzo, String modello, int memoria) throws Exception {
+        super(codiceProdotto, marca, prezzo);
+        this.modello = modello;
+        if (memoria <= 0) {
+            throw new IllegalArgumentException("La memoria deve essere > 0");
+        }
+        this.memoria = memoria;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+
+    @Override
+    public double calcolaPrezzo() {
+        return (int) (prezzo + memoria * prezzoRam);
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone: " + super.toString() + ", memoria=" + memoria +
+                ", modello='" + modello + "'";
+    }
+}
